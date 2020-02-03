4cbc162b94bbd759fca049bd2d3c2bba4f4cdc96
linuxuser <Mahajack>
2020-02-03 14:20:47 +0530

    Changes

--- Sums_In_Triangle.cs
+++ Sums_In_Triangle.cs
@@ -1,51 +1,117 @@
 using System;
-
-namespace SuminTriangle
+namespace LargestSumsInTriangle
+{ 
+public class SumInTriangle
 {
-    class MaximumSum
-    {
-        static void Main(string[] args)
-        {
-           
-            int row, column,row_value;
-            Console.WriteLine("Enter the Number of Rows and Columns : ");
-            row_value = Convert.ToInt32(Console.ReadLine());
-            int[,] array = new int[100,100];
-            string[] number = new string[100];
-            
-            
-            Console.WriteLine("The Matrix is:");
-            for ( row = 0; row < row_value; row++)
-            {
-                number[row] = Console.ReadLine();
-                for (column = 0; column <= row-2; column++)
-                {
-                   array[row,column] = Int32.Parse(number[row].Split(' ')[column]);   
-                }      
-            }
+       //First getting input elements as a string
+         string[] RowElements = new string[100];
+       //Formatting the input elements as a Array
+          int[,]Array = new int[100,100];
+          int RowIndex,ColumnIndex,NumberOfRows;
 
-            /*loop for sum of maximum number calculation*/
-            for ( row = row_value - 1; row >= 0; row--) 
-            { 
-            for (column = 0; column <= row; column++) 
-                {
-                 if (array[row + 1,column] >  
-                    array[row + 1,column + 1])
-                    {
-                       array[rows,column] += array[row + 1,column];        
-                    }  
-                else
-                   {
-                       array[row,column] += array[row + 1,column + 1]; 
-                   }
-                }
+   public  void GetInputValues()
+   {
+      NumberOfRows = -1;
+      Console.WriteLine("Enter the Number of Rows:\t");
+      while( NumberOfRows<1)
+      {
+         NumberOfRows = Convert.ToInt32(Console.ReadLine());
+         if(NumberOfRows<1)
+         {
+             
+             Console.WriteLine("Enter an Positive Integer between 0 to 100");
+         }
+      }
+         Console.WriteLine("Enter the Elements of an Array:");
+         
+         for(RowIndex = 0;RowIndex < NumberOfRows; RowIndex++)
+         {
+            RowElements[RowIndex] = Console.ReadLine();
+             for(ColumnIndex = 0;ColumnIndex <= RowIndex; ColumnIndex++)
+            {
+            Array[RowIndex,ColumnIndex] = Int32.Parse(RowElements[RowIndex].Split(' ')[ColumnIndex]);
             }
-            Console.Write("\nSums in a Triangle is : ");
-            Console.Write("{0}\t",array[0,0]);
-            Console.Write("\n");
-           }
-       }
+        } Console.WriteLine("");
     }
+   
+    public void ToCalculateMaximumPathSum()
+    {
+        // Adding the element of row 1
+        // to both the elements of row 2
+        // to reduce a step from the loop
+        if (NumberOfRows > 1)
+            Array[1, 1] = Array[1, 1] +
+                        Array[0, 0];
+            Array[1, 0] = Array[1, 0] +
+                        Array[0, 0];
+   
+        // Traverse remaining rows
+        for(int RowIndex = 2; RowIndex < NumberOfRows; RowIndex++)
+        {
+            Array[RowIndex, 0] = Array[RowIndex, 0] +
+                        Array[RowIndex - 1, 0];
+            Array[RowIndex, RowIndex] = Array[RowIndex, RowIndex] +
+                        Array[RowIndex - 1, RowIndex - 1];
+   
+            //Loop to traverse columns
+            for (int ColumnIndex = 2; ColumnIndex < RowIndex; ColumnIndex++)
+            {
+   
+                // Checking the two conditions,
+                // directly below and below right.
+                // Considering the greater one
+               
+                // tri[i] would store the possible
+                // combinations of sum of the paths
+                if (Array[RowIndex, ColumnIndex] + Array[RowIndex - 1, ColumnIndex - 1] >=
+                    Array[RowIndex, ColumnIndex] + Array[RowIndex - 1, ColumnIndex])
+               
+                    Array[RowIndex, ColumnIndex] = Array[RowIndex, ColumnIndex] +
+                                Array[RowIndex - 1, ColumnIndex - 1];
+                   
+                else
+                    Array[RowIndex, ColumnIndex] = Array[RowIndex, ColumnIndex] +
+                                Array[RowIndex - 1, ColumnIndex];
+            }
+        }
+       
+        // array at n-1 index (array[i])
+        // stores all possible adding
+        // combination, finding the
+        // maximum one out of them
+        int max = Array[NumberOfRows - 1, 0];
+       
+        for(int RowIndex = 1; RowIndex < NumberOfRows; RowIndex++)
+        {
+            if(max < Array[NumberOfRows - 1, RowIndex])
+                max = Array[NumberOfRows - 1, RowIndex];
+        }
+        Console.Write("The Sums in a Triangle is: ");
+        Console.WriteLine(max);
+    }
+    public static void Main()
+    { 
+        SumInTriangle sum = new SumInTriangle();
+        int Iteration, IterationIndex;
+        Iteration = -1;
+        while(Iteration<1)
+        {
+        Console.WriteLine("Enter the number of Testcases:");
+        Iteration = Convert.ToInt32(Console.ReadLine());
+        if(Iteration<1)
+         {
+            Console.WriteLine("Enter an Positive Integer: ");
+         }
+        }
+        for(IterationIndex=1;IterationIndex<=Iteration;IterationIndex++)
+        {
+        sum.GetInputValues();
+        sum.ToCalculateMaximumPathSum();
+        }
+}
+}
+}
+
 
 
 
